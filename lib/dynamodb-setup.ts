c0b804522ad81782f42549f04a49f09038de494c
Enuly <emanuelle.lorrany13@gmail.com>
2025-02-17 16:59:51 -0300

    refactor: :recycle: refactored dynamoDB Setup

--- lib/dynamodb-setup.ts
+++ lib/dynamodb-setup.ts
@@ -1,29 +1,29 @@
-import {aws_dynamodb as dynamodb, Stack} from 'aws-cdk-lib'
+import { aws_dynamodb, Stack } from "aws-cdk-lib";
 
-export class DynamoDBSetup{
-    private userTable:dynamodb.Table;
-    private stack:Stack
 
-    constructor(stackParameter: Stack){
-        this.stack = stackParameter
+export class DynamoDBSetup {
+    private userTable: aws_dynamodb.Table;
+    private stack: Stack;
+
+    constructor( stack:Stack ) {
+        this.stack = stack;
     }
 
-
-    setupUserTable():void{
-        this.userTable = new dynamodb.Table(this.stack,'UserTableIdentifier',{
-            partitionKey:{
-                name:"PK",
-                type:dynamodb.AttributeType.STRING
+    setUserTable(): void {
+        this.userTable = new aws_dynamodb.Table(this.stack, 'UserTable', { 
+            tableName:'UserTable', 
+            partitionKey: {
+                name:'PK',
+                type:aws_dynamodb.AttributeType.STRING
             },
-            sortKey:{
-                name:"SK",
-                type:dynamodb.AttributeType.STRING
+            sortKey: {
+                name:'SK',
+                type:aws_dynamodb.AttributeType.STRING
             },
-            billingMode:dynamodb.BillingMode.PAY_PER_REQUEST
+            billingMode:aws_dynamodb.BillingMode.PAY_PER_REQUEST
         })
     }
-
-    getUserTable():dynamodb.Table{
-        return this.userTable;
+    getUserTable(): aws_dynamodb.Table {
+        return this.userTable 
     }
 }
--- lib/svc-user-stack.ts
+++ lib/svc-user-stack.ts
@@ -1,13 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { DynamoDBSetup } from './dynamodb-setup';
+import { DynamoDBSetup } from './dynamodb-setup'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class SvcUserStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     //---------Setup DynamoDB----------//
-    const dynamodbSetup = new DynamoDBSetup(this)
-    dynamodbSetup.setupUserTable()
+    const dynamoDBSetup = new DynamoDBSetup(this)
+    dynamoDBSetup.setUserTable()
   }
 }
